aa3fcf7b1ea652699e1be67ea1e377b862f4d72e
Mason Garrett <thegarretts99@gmail.com>
2022-01-25 09:41:01 -0500

    - Rename ContentView - Organize files into folders - Add ContentModel - Set ContentModel as EnvironmentObject

--- LearningApp/LearningApp.swift
+++ LearningApp/LearningApp.swift
@@ -1,5 +1,5 @@
 //
-//  LearningAppApp.swift
+//  LearningApp.swift
 //  LearningApp
 //
 //  Created by Mason Garrett on 2022-01-25.
@@ -8,10 +8,11 @@
 import SwiftUI
 
 @main
-struct LearningAppApp: App {
+struct LearningApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
+                .environmentObject(ContentModel())
         }
     }
 }
---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Mason Garrett on 2022-01-25.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+}
