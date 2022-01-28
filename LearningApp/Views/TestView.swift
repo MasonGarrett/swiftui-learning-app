b2f933cfad093ac9eaecc149244e8b41a4ab7611
Mason Garrett <thegarretts99@gmail.com>
2022-01-28 17:41:37 -0500

    - nextQuestion method in the ContentModel - Dynamic button text in the TestView - Advance to the next question in TestView

--- LearningApp/Views/TestView.swift
+++ LearningApp/Views/TestView.swift
@@ -88,14 +88,25 @@ struct TestView: View {
                 // Submit Button
                 Button {
                     
-                    // Change submitted state to true
-                    submitted = true
-                    
-                    // Check the answer and increment the counter if correct
-                    if selectedAnswerIndex == model.currentQuestion!.correctIndex {
-                        numCorrect += 1
+                    // Check if answer has been submitted
+                    if submitted {
+                        // Answer has already been submitted, move to next question
+                        model.nextQuestion()
+                        
+                        // Reset properties
+                        submitted = false
+                        selectedAnswerIndex = nil
+                    } else {
+                        // Submit the answer
+                        
+                        // Change submitted state to true
+                        submitted = true
+                        
+                        // Check the answer and increment the counter if correct
+                        if selectedAnswerIndex == model.currentQuestion!.correctIndex {
+                            numCorrect += 1
+                        }
                     }
-                    
                 } label: {
                     
                     ZStack {
@@ -103,7 +114,7 @@ struct TestView: View {
                         RectangleCard(color: .green)
                             .frame(height: 48)
                         
-                        Text("Submit")
+                        Text(buttonText)
                             .bold()
                             .foregroundColor(Color.white)
                     }
@@ -119,6 +130,22 @@ struct TestView: View {
             ProgressView()
         }
         
+    }
+    
+    var buttonText: String {
+        
+        // Check if answer has been submitted
+        if submitted {
+            if model.currentQuestionIndex + 1 == model.currentModule!.test.questions.count {
+                // This is the last question
+                return "Finish"
+            } else {
+                // There is a next question
+                return "Next"
+            }
+        } else {
+            return "Submit"
+        }
     }
 }
 
